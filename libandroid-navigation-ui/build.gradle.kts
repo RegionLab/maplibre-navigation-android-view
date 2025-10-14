340b269155f9ebfa558d81c8a396226b21afc7a9
takhirbro <takhir.004@gmail.com>
2025-10-14 12:01:32 +0200

    update versions

--- libandroid-navigation-ui/build.gradle.kts
+++ libandroid-navigation-ui/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         compileSdk = 35
-        minSdk = 21
+        minSdk = 23
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
